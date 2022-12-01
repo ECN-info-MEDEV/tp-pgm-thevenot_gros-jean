f8d8cdaf309fd8f3bc1ecaefae66e9adcde9dfb1
vgros-jean <vince67000@gmail.com>
2022-12-01 11:06:27 +0100

    ajout d'un constructeur qui prend une matrice en argument

--- src/main/java/coursobjet/medevtp3/Image.java
+++ src/main/java/coursobjet/medevtp3/Image.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package coursobjet.medevtp3;
+import java.io.*;
 
 
 /**
@@ -40,10 +41,6 @@ public class Image {
         this.hauteur = 0;
         this.pixels = new int[this.largeur][this.hauteur];
     }
-    
-    
-    
-    //TODO méthode de lecture d'une image à partir d'un fichier
 
     /**
      * constructeur ou on donne une matrice
@@ -56,8 +53,44 @@ public class Image {
         this.pixels = new int[this.largeur][this.hauteur];
         for (int i=0; i<this.largeur; i++ ){
             System.arraycopy(pixels[i], 0, this.pixels[i], 0, this.hauteur);
-        }
-        
+        }  
     }
     
+    //TODO méthode de lecture d'une image à partir d'un fichier
+    
+    public static Image lire(String chemin) {
+        
+        try
+      {
+      // Le fichier d'entrée
+      File file = new File(chemin);    
+      // Créer l'objet File Reader
+      FileReader fr = new FileReader(file);  
+      // Créer l'objet BufferedReader        
+      BufferedReader br = new BufferedReader(fr);  
+      StringBuffer sb = new StringBuffer();    
+      String line;
+      while((line = br.readLine()) != null)
+      {
+        // ajoute la ligne au buffer
+        sb.append(line);      
+        sb.append("\n");     
+      }
+      fr.close();    
+      System.out.println(sb.toString());  
+    }
+    catch(IOException e)
+    {
+      e.printStackTrace();
+    }
+        
+        int[][] pixels = new int[1][1]; //TODO lire largeur et hauteur pour construire ici
+        
+        //TODO construire la matrice
+        
+        Image image = new Image(pixels);
+        return image;
+    }
+    
+    //TODO méthode pour écrire un fichier
 }
